0cc86da301956e082f759233fb1bda5e642bd28b
Your Name <you@example.com>
2021-06-11 22:12:01 +0900

    Create 4. insertion_sort.py

---
+++ 4. insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        for j in range(i):
+            if list[i] < list[j]:
+                temp = list[j]
+                list[j] = list[i]
+                list[i] = temp
+
+    return list
+
+print(insertion_sort([4,5,1,7,13,9,2,14,11,1,5,3,2,6]))
