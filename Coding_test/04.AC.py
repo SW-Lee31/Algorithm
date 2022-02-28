e323808ebd807ff918ae3a08cf32911e585bbb3c
SW-Lee31 <sangwon3232@naver.com>
2022-02-28 17:38:00 +0900

    Unfinished 04.AC

--- Coding_test/04.AC.py
+++ Coding_test/04.AC.py
@@ -1,3 +1,6 @@
+wind_flow_dir = ['up', 'down', 'left', 'right']
+wind_flow_seq = ['up', 'down', 'left', 'right']
+
 class Seat:
     def __init__(self, attr) -> None:
         self.attr = attr
@@ -10,21 +13,21 @@ class Seat:
         self.left = None
         self.right = None
 
-    def submit_xy(self, y_co, x_co, y_limit, x_limit,):
+    def submit_xy(self, map, y_co, x_co, y_limit, x_limit,):
         self.y_co = y_co
         self.x_co = x_co
         
         if self.x_co != 0:
-            self.up = [self.x_co - 1, self.y_co]
+            self.up = map[self.x_co - 1][self.y_co]
 
         if self.x_co < x_limit:
-            self.down = [self.x_co + 1, self.y_co]
+            self.down = map[self.x_co + 1][self.y_co]
 
         if self.y_co != 0:
-            self.left = [self.x_co, self.y_co - 1]
+            self.left = map[self.x_co][self.y_co - 1]
 
         if self.y_co < y_limit:
-            self.right = [self.x_co, self.y_co + 1]
+            self.right = map[self.x_co][self.y_co + 1]
 
     #def change_status(self, map):
         
@@ -40,13 +43,62 @@ class Seat:
 
     def get_around(self):
         return self.up, self.down, self.left, self.right
+
+class wind_flow:
+    global wind_flow_dir
+    def __init__(self, ac) -> None:
+        self.seat_list = []
+        self.detect_list = []
+        self.root = ac
+        self.current = ac
+        self.next = None
+        self.seq = wind_flow_seq[0]
+        self.dir = None
+
+        if self.current is not None:
+            self.seat_list.append(self.current)
+            self.detect_list.append(self.current)
+        else:
+            print('에어컨이 입력되지 않았습니다.')
+            return
+
+    def cal(self):
+        if self.seq == wind_flow_seq[0]:
+            self.dir = wind_flow_dir[0]
+            if self.current.up is not None:
+                if self.current.up.attr == 0:
+                    if self.current.up not in self.detect_list:
+                        self.next = self.current.up
+                    elif self.current.up.attr == 1:
+                        self.next = self.current.up
+                    elif self.current.up.attr == 2:
+                        self.dir = None
+                        self.seq = wind_flow_seq[1]
+                        return
+                    elif self.current.up.attr == 3:
+                        self.dir = wind_flow_dir[3]
+                    elif self.current.up.attr == 4:
+                        self.dir = wind_flow_dir[2]
+
+                    
+            
+        
+        if self.next != None:
+            self.current = self.next
+            self.seat_list.append(self.current)
+            self.detect_list.append(self.current)
+        
+        print(len(self.seat_list))
+        self.cal()
+        return 
+
     
 def input_2d(x_amount, y_amount):
     map = [[Seat(0) for y in range(y_amount)] for x in range(x_amount)]
     
     for i in range(len(map)):
         for j in range((len(map[i]))):
-            map[i][j].submit_xy(j, i, (y_amount - 1), (x_amount - 1))
+            map[i][j].submit_xy(map, j, i, (y_amount - 1), (x_amount - 1))
 
     return map
 
@@ -55,7 +107,7 @@ def print_map(map):
         for j in range((len(map[i]))):
             attr_digit = map[i][j].get_attr()
             x_co, y_co = map[i][j].get_xy()
-            print('{0}{1} : {2}'.format(x_co, y_co, attr_digit), end=' ')
+            print('{0}{1} : {2}'.format(x_co, y_co, attr_digit), end='\t')
         print('\n')
     
 def print_status(map):
@@ -68,11 +120,12 @@ def print_status(map):
 
 def ask_ac(map, x_co, y_co):
     map[x_co][y_co].set_attr(9)
+    return map[x_co][y_co]
 
 def ask_tool(map, amount):
     for i in range(amount):
         attr, x, y = input('{0}번째의 물건번호과 좌표를 입력해주세요 : '.format(i + 1)).split(' ')
-        map[x][y].set_attr(attr)
+        map[int(x)][int(y)].set_attr(attr)
 
 def cal_map(map):
     for i in range(len(map)):
@@ -80,9 +133,12 @@ def cal_map(map):
             attr_digit = map[i][j].get_attr()
 
 if __name__ == '__main__':
-    map = input_2d(2, 3)
+    map = input_2d(4, 4)
     print_map(map)
     print_status(map)
-    #ask_ac(map, 0, 1)
-    #print_map(map)
+    ac = ask_ac(map, 0, 1)
+    ask_tool(map, 2)
+    print_map(map)
+    flow = wind_flow(ac)
+    flow.cal()
 
